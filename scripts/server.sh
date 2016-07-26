addd18eaf93f9b764d8399bfb5ea024eb517152c
Felipe Pantoja <felipempantoja@gmail.com>
2016-07-26 10:13:19 -0300

    variaveis de ambiente

---
+++ scripts/server.sh
@@ -0,0 +1,7 @@
+export NODE_ENV=development
+export SELENIUM_USER=pantojatest
+export SELENIUM_USER_PASSWORD=panta123
+export CLIENT_ID=4adfbebc638fcbecf270
+export CLIENT_SECRET=09bf50b7c260a972f78a8ccba427639564b357f0
+
+node server
