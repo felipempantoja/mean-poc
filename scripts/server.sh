3fb9a482abf4719c7587da444dc3a7df6708eee9
Felipe Pantoja <felipempantoja@gmail.com>
2016-07-26 10:55:14 -0300

    Removed scripts folder

--- scripts/server.sh
+++
@@ -1,7 +0,0 @@
-export NODE_ENV=development
-export SELENIUM_USER=pantojatest
-export SELENIUM_USER_PASSWORD=pantoja123
-export CLIENT_ID=4adfbebc638fcbecf270
-export CLIENT_SECRET=09bf50b7c260a972f78a8ccba427639564b357f0
-
-node server
